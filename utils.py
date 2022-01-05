326876bb6cb79ef625cb85f257458357b862bf1e
Kai <56094206+kailliang@users.noreply.github.com>
2022-01-05 19:28:47 +0000

    Update utils.py

--- utils.py
+++ utils.py
@@ -4,16 +4,16 @@ from pytorchvideo.transforms import ApplyTransformToKey, UniformTemporalSubsampl
 from torch import nn
 from torchvision.transforms import Compose, Lambda, RandomCrop, RandomHorizontalFlip, CenterCrop
 
-side_size = 400
-max_size = 400
+side_size = 256
+max_size = 320
 mean = [0.45, 0.45, 0.45]
 std = [0.225, 0.225, 0.225]
-crop_size = 400
+crop_size = 256
 num_frames = 32
-sampling_rate = 1
-frames_per_second = 32/6
+sampling_rate = 2
+frames_per_second = 30
 clip_duration = (num_frames * sampling_rate) / frames_per_second
-num_classes = 3
+num_classes = 400
 
 
 class PackPathway(nn.Module):
@@ -36,7 +36,8 @@ class PackPathway(nn.Module):
 
 train_transform = ApplyTransformToKey(key="video", transform=Compose(
     [UniformTemporalSubsample(num_frames), Lambda(lambda x: x / 255.0), Normalize(mean, std),
-     RandomShortSideScale(min_size=side_size, max_size=max_size), RandomCrop(crop_size), PackPathway()]))
+     RandomShortSideScale(min_size=side_size, max_size=max_size), RandomCrop(crop_size),
+     PackPathway()]))
 
 test_transform = ApplyTransformToKey(key="video", transform=Compose(
     [UniformTemporalSubsample(num_frames), Lambda(lambda x: x / 255.0), Normalize(mean, std),
